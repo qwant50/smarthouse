9da2be7d5b2bcd9a2b172c92fba495a757eff870
qwant50 <qwantonline@gmail.com>
2017-08-23 21:26:38 +0300

    fix text

--- builder.js
+++ builder.js
@@ -9,7 +9,9 @@ console.dir(television.state);
 console.dir(television);
 alert(television.channel);
 alert(television.toString());
+
+// set delay 1 min
 Timer.setDelay(1, () => {
     television.state = 'off';
-    alert('TV has been took off after 1 minute. Current state is: ' + television.state);
+    alert('TV has been taken off after 1 minute. Current state is: ' + television.state);
 });
